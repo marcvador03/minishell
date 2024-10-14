2a7b7f37d51de864c70d7731d4821822a15011b4
mfleury <mfleury@student.42barcelona.com>
2024-10-14 12:20:48 +0200

    corrected valgrind ignore + added history for shell

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: mfleury <mfleury@student.42barcelona.      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 10:01:23 by mfleury           #+#    #+#             */
-/*   Updated: 2024/10/14 11:33:40 by mfleury          ###   ########.fr       */
+/*   Updated: 2024/10/14 12:15:26 by mfleury          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,8 @@ void	get_cmd(char *input)
 {
 	char	**args;
 
+	if (input == NULL)
+		return ;
 	args = ft_split(input, ' ');
 	if (args == NULL)
 		exit(EXIT_FAILURE);
@@ -48,6 +50,8 @@ char	*get_input()
 	char	*line;
 
 	line = readline("#");
+	if (line != NULL && *line != '\0')
+		add_history(line);
 	return (line);	
 }
 
@@ -61,15 +65,18 @@ int	main(/*int argc, char *argv[], char *envp[]*/)
 	while (1)
 	{
 		input = get_input();
-		pid = fork();
-		if (pid == -1)
-			exit(EXIT_FAILURE);
-		if (pid == 0)
-			get_cmd(input);
-		if (waitpid(0, &wstatus, 0) == -1)
-			exit(EXIT_FAILURE);
-		if (WIFEXITED(wstatus) && WEXITSTATUS(wstatus) == 0)
-			exit(EXIT_SUCCESS);
+		if (input != NULL)
+		{
+			pid = fork();
+			if (pid == -1)
+				exit(EXIT_FAILURE);
+			if (pid == 0 && input != NULL)
+				get_cmd(input);
+			if (waitpid(0, &wstatus, 0) == -1)
+				exit(EXIT_FAILURE);
+			if (WIFEXITED(wstatus) && WEXITSTATUS(wstatus) == 0)
+				exit(EXIT_SUCCESS);
+		}
 		free(input);
 	}
 }
