7fd0e1642a507dadb847640b74de08fed7e2d84f
Pau Anand Morelló Amorós <pmorello@car10s6.42barcelona.com>
2024-10-14 12:05:25 +0200

    minishell

--- include/minishell.h
+++ include/minishell.h
@@ -6,16 +6,21 @@
 /*   By: mfleury <mfleury@student.42barcelona.      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 10:00:44 by mfleury           #+#    #+#             */
-/*   Updated: 2024/10/14 10:08:38 by mfleury          ###   ########.fr       */
+/*   Updated: 2024/10/14 12:04:56 by pmorello         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
 # include "../libft/libft.h"
 # include "../libft/ft_printf/ft_printf.h"
 # include "../libft/getnextline/get_next_line.h"
 
+extern char **environ; //variable global para la funcion de ft_env
 
 #endif
