05c0c95a048255b080d9ac988b94c82d63b15481
Pau Anand Morelló Amorós <pmorello@car10s6.42barcelona.com>
2024-10-14 12:11:40 +0200

    built_ins

--- src/built_ins.c
+++ src/built_ins.c
@@ -6,11 +6,13 @@
 /*   By: pmorello <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 10:36:03 by pmorello          #+#    #+#             */
-/*   Updated: 2024/10/14 11:21:18 by pmorello         ###   ########.fr       */
+/*   Updated: 2024/10/14 12:11:17 by pmorello         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+
+//void echo -n
 
 void	ft_cd(char *path)
 {
@@ -30,6 +32,30 @@ void	ft_pwd(int argc)
 	printf("%s\n", buffer);
 	free (buffer);
 	if (argc > 1)
-		printf("pwd : too many arguments");
+		printf("pwd : too many arguments\n");
 	
+}
+
+//void	ft_export
+
+//void	ft_unset
+
+void	ft_env(int argc, char **argv)
+{
+	//usar la variable global environ
+	char **env;
+	
+	env = environ;
+	while (*env)
+	{
+		printf("%s\n", *env);
+		env++;
+	}
+	if (argc > 1)
+		printf("env: '%s' : No such file or directori\n", argv[1]);
+}
+
+void	ft_exit(void)
+{
+	exit(0);
 }
