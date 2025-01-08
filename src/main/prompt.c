0359d66e8f5b1654a6e3e64effbbd5eb8c1b5f87
Marc Fleury <mfleury03@gmail.com>
2025-01-09 00:22:29 +0100

    finalized prompt with fallbacks

--- src/main/prompt.c
+++ src/main/prompt.c
@@ -6,7 +6,7 @@
 /*   By: mfleury <mfleury@student.42barcelona.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 20:31:15 by mfleury           #+#    #+#             */
-/*   Updated: 2025/01/08 23:06:59 by mfleury          ###   ########.fr       */
+/*   Updated: 2025/01/09 00:15:50 by mfleury          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,13 +41,17 @@ char	*join_prompt(t_prompt *t)
 	return (res);
 }
 
-static char	*replace_home_path(char *ex_path, char *home)
+static char	*replace_home_path(char *home)
 {
+	char	*ex_path;
 	char	*res;
 	int		len[2];
 		
+	ex_path = getcwd(NULL, 0);
+	if (ex_path == NULL)
+		return (set_gstatus(202), NULL);
 	if (home == NULL || *home == '\0')
-		return (ft_strdup(ex_path));
+		return (ex_path);
 	len[0] = ft_strlen(ex_path);
 	len[1] = ft_strlen(home);
 	if (len[0] >= len[1] && home[0] == '/' && home[len[1] - 1] != '/')
@@ -56,43 +60,75 @@ static char	*replace_home_path(char *ex_path, char *home)
 		{
 			res = (char *)ft_calloc(sizeof(char), len[0] - len[1] + 2);
 			if (res == NULL)
-				return (set_gstatus(202), ft_strdup(ex_path));
+				return (set_gstatus(202), ex_path);
 			res[0] = '~';
 			ft_memcpy(res + 1, ex_path + len[1], len[0] - len[1]);
 			res[len[0] - len[1] + 1] = '\0';
-			return (res);
+			return (free_s(ex_path), res);
 		}
 	}
-	return (ft_strdup(ex_path));
+	return (ex_path);
 }
 
+static char	*get_user(t_env *env)
+{
+	char		*user;
+	int			slot;
+	int			fd;
+	struct utmp ut;
+	int 		len;
+
+	user = sh_getenv(env, "USER");
+	if (user != NULL)
+		return (user);
+	slot = ttyslot();
+	user = NULL;
+	len = sizeof(struct utmp);
+	fd = open(UTMP_FILE, O_RDONLY);
+	if (fd == -1)
+		return ("unk");
+	while (read(fd, &ut, len) == len)
+	{
+		if (ft_atoi(ut.ut_id + 1) == slot) 
+			user = ut.ut_user;
+		printf("%s\n", ut.ut_id + 1);
+	}
+	return (user);
+}
+
+static char	*get_hostname()
+{
+	int		fd;
+	char	buf[65];
+	char	*host;
+
+	fd = open("/etc/hostname", O_RDONLY);
+	if (fd == -1)
+		return (NULL);
+	if (read(fd, &buf, 64) > 1)
+	{
+		buf[64] = '\0';
+		host = ft_strtrim(buf, "\n");
+		if (host == NULL)
+			return (set_gstatus(202), NULL);
+	}	return ((host));
+	return (NULL);
+}
 char	*create_prompt(t_env *env)
 {
 	t_prompt	t;
 
-	t.user = sh_getenv(env, "USER");
-	t.hostname = NULL;
-	t.ex_cur_path = sh_getenv(env, "PWD");
+	t.user = get_user(env);
+	t.hostname = get_hostname();
 	t.home = sh_getenv(env, "HOME");
-	t.cur_path = replace_home_path(t.ex_cur_path, t.home);
+	t.cur_path = replace_home_path(t.home);
 	t.status = ft_itoa(g_status);
 	t.prompt = join_prompt(&t);
-	return (free_s(t.status), free_s(t.cur_path), t.prompt);
+	free_s(t.status);
+	free_s(t.cur_path);
+	free_s(t.hostname);
+	return (t.prompt);
 }
 /*
-	int	x;
-	int	fd;
-	struct utmp ut;
-	int 	len;
-	x = ttyslot();
-	len = sizeof(struct utmp);
-	fd = open(UTMP_FILE, O_RDONLY);
-	if (fd == -1)
-		ft_putstr_fd("error opening utmp", 2);
-	while (read(fd, &ut, len) == len)
-	{
-		printf("%s ", ut.ut_user);
-		printf("%s\n", ut.ut_id + 1);
-	}
 */
 
