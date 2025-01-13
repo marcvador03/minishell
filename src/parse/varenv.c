cc7e8fbb4825d9a5eeeccc0ba95d40c9a511c6fc
Marc Fleury <mfleury03@gmail.com>
2025-01-13 20:53:04 +0100

    corrected variable env display, passed all tests

--- src/parse/varenv.c
+++ src/parse/varenv.c
@@ -6,7 +6,7 @@
 /*   By: mfleury <mfleury@student.42barcelona.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 16:34:27 by mfleury           #+#    #+#             */
-/*   Updated: 2025/01/13 18:56:59 by mfleury          ###   ########.fr       */
+/*   Updated: 2025/01/13 20:45:21 by mfleury          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,9 +54,14 @@ static char	*expand_getenv(char *s, t_env *env)
 }
 
 
-static int	remove_dollar(char **line, int i, char *dollar_in)
+static int	remove_dollar(char **line, int i, char *dollar_in, int flag)
 {
-	if (ft_isalnum_plus((*line)[i + 1]) == 1)
+	if (ft_isalnum_plus((*line)[i + 1]) == 1 && flag != -1)
+	{
+		ft_memset(*line + i, ' ', 1);
+		return (0);
+	}
+	else if (flag != -1 && ((*line)[i + 1] ==34 || (*line)[i + 1] == 39))
 	{
 		ft_memset(*line + i, ' ', 1);
 		return (0);
@@ -69,7 +74,7 @@ static int	remove_dollar(char **line, int i, char *dollar_in)
 	return (0);
 }
 
-static char	*expand_env_loop(t_env *env, char *line, int *i, int l_status)
+static char	*expand_env_loop(t_env *env, char *line, int *i, int flags[2])
 {
 	char	*res;
 	char	*dollar_out;
@@ -80,10 +85,10 @@ static char	*expand_env_loop(t_env *env, char *line, int *i, int l_status)
 	if (dollar_in == NULL)
 		return (NULL);
 	if (dollar_in[0] == '?')
-		dollar_out = ft_itoa(l_status);
+		dollar_out = ft_itoa(flags[1]);
 	else if (dollar_in[0] == '_' && dollar_in[1] == '\0')
 		dollar_out = ft_strdup(sh_getenv(env, "_"));
-	else if (remove_dollar(&line, *i, dollar_in) == -1)
+	else if (remove_dollar(&line, *i, dollar_in, flags[0]) == -1)
 		return (line);
 	else if (ft_isalnum(line[*i + 1]) == 1)
 		dollar_out = expand_getenv(dollar_in, env);
@@ -96,28 +101,28 @@ static char	*expand_env_loop(t_env *env, char *line, int *i, int l_status)
 char	*expand_env(t_env *env, char *line, int x, int l_status)
 {
 	int	i;
-	int	flag;
+	int	flag[2];
 
-	i = 0;
-	flag = x;
-	while (line[i] != '\0')
+	i = -1;
+	flag[0] = x;
+	flag[1] = l_status;
+	while (line[++i] != '\0')
 	{
 		if (line[i] == 34)
-			flag = flag * (-1);
-		while (line[i] == 39 && flag >= 0)
+			flag[0] = flag[0] * (-1);
+		while (line[i] == 39 && flag[0] >= 0)
 			i += sh_jump_to(line + i, line[i]);
 		if (line[i] == '\0')
 			return (line);
-		if ((line[i] == '$' && flag != -1) || \
+		if ((line[i] == '$' && flag[0] != -1) || \
 			(line[i] == '$' && i > 0 && line[i - 1] != '\\'))
 		{
-			line = expand_env_loop(env, line, &i, l_status);
+			line = expand_env_loop(env, line, &i, flag);
 			if (line == NULL)
 				return (flush_errors("", 202), NULL);
 			if (line[i] == '\0')
 				return (line);
 		}
-		i++;
 	}
 	return (line);
 }
