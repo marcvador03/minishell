b963b9606eb0ad4d0c5e15cbe5199b2285a16045
Marc Fleury <mfleury03@gmail.com>
2025-01-27 13:10:57 +0100

    change forbidden character logic

--- src/main/shell.c
+++ src/main/shell.c
@@ -6,7 +6,7 @@
 /*   By: mfleury <mfleury@student.42barcelona.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 15:12:52 by mfleury           #+#    #+#             */
-/*   Updated: 2025/01/25 20:16:30 by mfleury          ###   ########.fr       */
+/*   Updated: 2025/01/27 13:07:19 by mfleury          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,21 @@ static int	check_forbidden_c(char *line)
 	i = 0;
 	while (line[i] != 0)
 	{
-		if (line[i] == ';' || line[i] == 92 || line[i] == 10 || line[i] == 42)
+		if ((line[i] > 0 && line[i] <= 31) || line[i] == 92)
+			return (flush_errors("", 210, line[i]), -1);
+		i++;
+	}
+	i = 0;
+	while (line[i] != 0)
+	{
+		while (line[i] == 34 || line[i] == 39)
 		{
-			flush_errors("", 210, line[i]);
-			return (-1);
+			i += sh_jump_to(line + i, line[i]);
+			if (line[i] == '\0')
+				return (0);
 		}
+		if (line[i] == ';' || line[i] == 42)
+			return (flush_errors("", 210, line[i]), -1);
 		i++;
 	}
 	return (0);
