111de87eb48d6aa54e9f28c60c71016994a22ec6
Marc Fleury <mfleury03@gmail.com>
2025-01-27 15:43:29 +0100

    fixed leak

--- src/parse/variable_env.c
+++ src/parse/variable_env.c
@@ -6,7 +6,7 @@
 /*   By: mfleury <mfleury@student.42barcelona.      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 12:19:07 by mfleury           #+#    #+#             */
-/*   Updated: 2025/01/27 15:30:02 by mfleury          ###   ########.fr       */
+/*   Updated: 2025/01/27 15:42:58 by mfleury          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,6 @@ static char	*resize_line(char *line, char *out, char *in, int *i)
 	char	*tmp[3];
 	int		len[4];
 
-	/*if (out == NULL)
-		return (free_s(line), NULL);*/
 	len[0] = ft_strlen(in);
 	len[1] = ft_strlen(line);
 	len[2] = ft_strlen(out);
@@ -31,7 +29,12 @@ static char	*resize_line(char *line, char *out, char *in, int *i)
 	*i += len[2];
 	res = ft_strjoin(tmp[2], tmp[1]);
 	if (res == NULL || res[0] == '\0')
-		return (free_s(res), NULL);
+	{
+		free_s(line);
+		free_s(out);
+		free_s(res);
+		return (free_s(tmp[0]), free_s(tmp[1]), free_s(tmp[2]), NULL);
+	}
 	free_s(out);
 	free_s(line);
 	return (free_s(tmp[0]), free_s(tmp[1]), free_s(tmp[2]), res);
