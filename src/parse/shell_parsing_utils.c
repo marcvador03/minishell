fe80292b667743edb8e85ecde32721532305b3cb
Marc Fleury <mfleury03@gmail.com>
2025-01-29 16:10:28 +0100

    corrected shell parsing with > and |

--- src/parse/shell_parsing_utils.c
+++ src/parse/shell_parsing_utils.c
@@ -6,7 +6,7 @@
 /*   By: mfleury <mfleury@student.42barcelona.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 16:10:10 by mfleury           #+#    #+#             */
-/*   Updated: 2025/01/28 16:13:15 by mfleury          ###   ########.fr       */
+/*   Updated: 2025/01/29 15:17:39 by mfleury          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,7 @@ static int	check_tokens_errors(char *line, t_parse *q, int *l_status)
 	return (0);
 }
 
-static int	get_next_rd(char *line, t_parse *q, int *l_status)
+static int	get_next_rd(t_shell *sh, char *line, t_parse *q, int *l_status)
 {
 	q->prev_pos = q->i;
 	while (oneofchar(line[q->i], "&,|,(,)") == FALSE && line[q->i] != '\0')
@@ -59,7 +59,7 @@ static int	get_next_rd(char *line, t_parse *q, int *l_status)
 		*l_status = flush_errors("", 210, line[q->prev_pos]);
 		return (-1);
 	}
-	return (0);
+	return (get_next_token(sh, line, q, l_status));
 }
 
 static int	get_next_dollar(t_shell *sh, char *line, t_parse *q, int *l_status)
@@ -89,7 +89,7 @@ int	get_next_token(t_shell *sh, char *line, t_parse *q, int *l_status)
 	q->tk = line[q->i];
 	if (line[q->i] == '>' || line[q->i] == '<')
 	{
-		if (get_next_rd(line, q, l_status) == -1)
+		if (get_next_rd(sh, line, q, l_status) == -1)
 			return (-1);
 	}
 	else if (line[q->i] == '&' || line[q->i] == '|')
